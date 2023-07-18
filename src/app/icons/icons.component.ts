558e62132345867b21f3a10d9d3524c6d4d516ed
mtsheda <tmafora2@gmail.com>
2023-07-18 12:50:44 +0200

    added ngFor

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class IconsComponent {
 
+  iconsData = [
+    { fontIcon: 'notifications', description: 'Notification' },
+    { fontIcon: 'security', description: 'Security' },
+    { fontIcon: 'theme', description: 'Theme' },
+    { fontIcon: 'chat wallpaper', description: 'Chat Wallpaper' },
+    { fontIcon: 'blocked', description: 'Blocked' },
+    { fontIcon: 'desktop settings', description: 'Desktop Settings' },
+    { fontIcon: 'keyboard shortcuts', description: 'Keyboard Settings' },
+    { fontIcon: 'help', description: 'Help' },
+  ];
+
 }
